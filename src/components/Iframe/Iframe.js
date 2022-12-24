6c58f1672c31338cc692f9bcdbe10667e1af4970
omakoji <ernestmoi@yahoo.com>
2022-12-24 11:03:17 +0100

    Modify vsl certificate iframe

--- src/components/Iframe/Iframe.js
+++ src/components/Iframe/Iframe.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Iframe() {
   return (
-    <div>
-        <div className="frame">
-            <div className="frameDiv">
-                <iframe frameBorder="0" title='vsl certificate' scrolling="no" allowtransparency="true" src="https://www.victoriasolicitorslimited.co.uk/iframe/55845r.html" className='vslFrame'></iframe>
-            </div>
-        </div>
+
+    <div style={{maxWidth: '275px', maxHeight: '163px'}}>
+      <div style={{position: 'relative', paddingBottom: '59.1%', height: 'auto', overflow: 'hidden'}}>
+        <iframe frameborder="0" scrolling="no" allowTransparency="true" src="https://www.victoriasolicitorslimited.co.uk/iframe/55845r.html"  style={{border: '0px', margin: '0px', padding: '0px', width:'100%', height:'100%', backgroundColor: 'transparent', top: '0px', left: '0px', position: 'absolute'}} title="victoria solicitors limited certificate">
+        </iframe>
+      </div>
     </div>
   )
 }
